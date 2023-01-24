colesbury/nogil

mimalloc: tag pages with qsbr counter
This changes mimalloc to tag pages for PyObjects with the current qsbr
"goal". These pages can't be freed or used in a different heap until
after the qsbr shared read counter reaches the goal.

Include/mimalloc/mimalloc-internal.h
@@ -426,6 +426,16 @@ static inline mi_page_t* _mi_ptr_page(void* p) {
   return _mi_segment_page_of(_mi_ptr_segment(p), p);
 }
 
+static inline void _mi_page_use_qsbr(mi_page_t* page) {
+  if (!mi_atomic_read8_relaxed(&page->use_qsbr)) {
+    mi_atomic_write8_relaxed(&page->use_qsbr, true);
+  }
+}
+
+static inline void _mi_ptr_use_qsbr(void* p) {
+  _mi_page_use_qsbr(_mi_ptr_page(p));
+}
+
 // Get the block size of a page (special cased for huge objects)
 static inline size_t mi_page_block_size(const mi_page_t* page) {
   const size_t bsize = page->xblock_size;

colesbury/nogil-3.12

Include/mimalloc/mimalloc-internal.h
@@ -522,6 +522,16 @@ static inline mi_page_t* _mi_ptr_page(void* p) {
   return _mi_segment_page_of(_mi_ptr_segment(p), p);
 }
 
+static inline void _mi_page_use_qsbr(mi_page_t* page) {
+  if (!mi_atomic_load_relaxed(&page->use_qsbr)) {
+    mi_atomic_store_relaxed(&page->use_qsbr, true);
+  }
+}
+
+static inline void _mi_ptr_use_qsbr(void* p) {
+  _mi_page_use_qsbr(_mi_ptr_page(p));
+}
+
 // Get the block size of a page (special case for huge objects)
 static inline size_t mi_page_block_size(const mi_page_t* page) {
   const size_t bsize = page->xblock_size;